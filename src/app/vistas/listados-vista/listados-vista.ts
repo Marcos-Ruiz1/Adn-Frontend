fe376a92a69322453eeef6e2a81be990462ae909
Marcos Zazueta Ruiz <marcos@192.168.1.21>
2026-02-01 11:41:08 -0700

    feat(ListadosFuncionalidad): se implemento la funcionalidad completa de listar las últimas peticiones, tanto html y lógica TS

--- src/app/vistas/listados-vista/listados-vista.ts
+++ src/app/vistas/listados-vista/listados-vista.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject} from '@angular/core';
+import {TablaAdn} from '@components/tabla-adn/tabla-adn';
+import {Adn} from '@core/modelos/Adn';
+import {AdnServicio} from '@core/servicios/AdnServicio';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-listados-vista',
-  imports: [],
+  standalone: true,
+  imports: [TablaAdn],
   templateUrl: './listados-vista.html',
   styleUrl: './listados-vista.css',
 })
-export class ListadosVista {
+export class ListadosVista  {
+
+  ListadoAdn: Adn[] = [];
+
+  private adnServicio = inject(AdnServicio);
+
+  // 2. Convert the Observable directly into a Signal.
+  // This automatically handles the subscription and unsubscription.
+  // 'listadoAdn' will be 'undefined' while loading.
+  listadoAdn = toSignal(this.adnServicio.obtenerListadoAdn());
+
+  constructor() {
+    // No logic needed in constructor or ngOnInit for basic data fetching anymore!
+  }
 
 }
