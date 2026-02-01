9a15f4e00acb38b4e1a8ac00079de99b2f4d6827
Marcos Zazueta Ruiz <marcos@192.168.1.21>
2026-01-31 19:57:40 -0700

    Configuración inicial proyecto front-end

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from "./compartidos/componentes/navbar/navbar";
+//import {AdnHomeVista} from "./vistas/adn-home-vista/adn-home-vista";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [/*RouterOutletAdnHomeVista*/Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AdnFrontend');
+  
 }
