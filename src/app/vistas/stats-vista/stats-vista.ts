d9a126edc06b18c7640d63a90209d8cf543e0c7b
Marcos Zazueta Ruiz <marcos@192.168.1.21>
2026-02-01 11:42:49 -0700

    feat(StatsFuncionalidad): se implemento la funcionalidad completa de mostrar las stats de los ADN's en la base de datos, tanto html y lógica TS

--- src/app/vistas/stats-vista/stats-vista.ts
+++ src/app/vistas/stats-vista/stats-vista.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Stats} from '@core/modelos/Stats';
+import {AdnServicio} from '@core/servicios/AdnServicio';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-stats-vista',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stats-vista.html',
   styleUrl: './stats-vista.css',
 })
 export class StatsVista {
+
+  stats!:Stats;
+
+  private adnServicio = inject(AdnServicio);
+
+  // 2. Convert the Observable directly into a Signal.
+  // This automatically handles the subscription and unsubscription.
+  statsSignal = toSignal(this.adnServicio.obtenerStatsAdn());
+
+  constructor() {
+    // No logic needed in constructor or ngOnInit for basic data fetching anymore!
+  }
 
 }
