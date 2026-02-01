a930b4128b9ad7ec2cb678188712dcd68d560ab3
Marcos Zazueta Ruiz <marcos@192.168.1.21>
2026-02-01 11:30:09 -0700

    feat(AdnServicio): se agregó las funciones faltantes para consumir la API

--- src/app/core/servicios/AdnServicio.ts
+++ src/app/core/servicios/AdnServicio.ts
@@ -3,14 +3,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Adn } from '@core/modelos/Adn';
+import { Stats } from '@core/modelos/Stats';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdnServicio {
 
-  private apiUrl = "TESTEST"
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
+  obtenerListadoAdn(): Observable<Adn[]> {
+    return this.http.get<Adn[]>(this.apiUrl + '/list');
+  }
+
+  obtenerStatsAdn(): Observable<Stats> {
+    return this.http.get<Stats>(this.apiUrl + '/stats');
+  }
+
+  verificarAdn(dna: string[]): Observable<any> {
+    return this.http.post(this.apiUrl + '/mutation', { "adnSecuencia":dna });
+  }
 }
